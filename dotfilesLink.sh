499f2a8e02d32f47f1d29afe058132299e325339
kinformaiton <kanatani.social@gmail.com>
2016-12-12 22:46:58 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/vim ~/.vim
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.gemrc ~/.gemrc
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.gemrc ~/.gemrc
+
+bash ~/.dotfiles/neobundle_install.sh
